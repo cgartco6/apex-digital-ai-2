b89049b63119c59c1f691a7f29424f29dbbfead4
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-02-04 15:27:48 +0200

    Update mysql.py

--- database/mysql.py
+++ database/mysql.py
@@ -1,9 +1,14 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
 
-def connect():
+load_dotenv()
+
+def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="DB_USER",
-        password="DB_PASS",
-        database="DB_NAME"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
     )
